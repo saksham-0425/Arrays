5c346a3c9581ba7341f6e13c100584896a950998
Saksham Gupta <124126440+saksham-0425@users.noreply.github.com>
2023-08-06 11:21:52 +0530

    Add files via upload

---
+++ arrayinsertionsort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+//insertion sort
+//----insert and element from unsorted array to its correct position in sorted array
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+    for(int i=1;i<n;i++){
+        int current=arr[i];
+        int j=i-1;
+        while(arr[j]>current && j>=0){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=current;
+    }
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<endl;
+    }
+
+    return 0;
+}
